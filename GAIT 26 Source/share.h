05fe32fd8524a75e0e1e6f2988bf6c30eaa5c068
pho3nixx-zz <12443383+pho3nixx-zz@users.noreply.github.com>
2018-08-13 20:46:29 +0200

    Add files via upload

---
+++ GAIT 26 Source/share.h
@@ -0,0 +1,10 @@
+#ifndef SHARE_H
+#define SHARE_H
+
+#include "share_linux.h"
+
+#define AFX_STDAFX_H__6E70A389_33B8_4286_AD98_3ECD475C4512__INCLUDED_ 
+//skip AFX stuff for linux
+#endif // SHARE_H
+
+
